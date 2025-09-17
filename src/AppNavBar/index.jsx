f02fba6416d0eb219b099e8d0d720ea06fe80bb7
ravvitkim <mirkim6086@naver.com>
2025-09-17 17:25:41 +0900

    가격 물건별 정렬기능 추가 및 스크롤 기능추가함

--- src/AppNavBar/index.jsx
+++ src/AppNavBar/index.jsx
@@ -1,13 +1,12 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Container, Nav, Navbar, NavDropdown } from "react-bootstrap";
-import './AppNavBar.css'
 // Route : 페이지(컴포넌트) 이동 처리
 // Routes : Route를 감싸는 용도
 // Link : 실제로 페이지를 보여주는 역할, Link위치에 컴포넌트 뿌려줌
 // userNavigate : 스크립트 영역에서 링크처리를 하는 훅
 import { Link, useNavigate } from "react-router-dom";
 
-function AppNavBar(){
+function AppNavBar({ sortByPriceAsc, sortByPriceDesc, sortByNameAsc,sortByNameDesc }) {
 
   const navigate = useNavigate();
 
@@ -18,16 +17,26 @@ function AppNavBar(){
           <Container>
             <Navbar.Brand>land</Navbar.Brand>
             <Nav className="me-auto">
-              <Nav.Link onClick={()=>{navigate('/')}}>Home</Nav.Link>
-              <Nav.Link>Cart</Nav.Link>
-              <Nav.Link onClick={()=>{navigate('/about')}}>About</Nav.Link>
-              <NavDropdown title="Info" id="basic-nav-dropdown">
-                <NavDropdown.Item onClick={()=>{navigate('/about/member')}}>Member Page</NavDropdown.Item>
-                <NavDropdown.Item onClick={()=>{navigate('/about/location')}}>Location</NavDropdown.Item>
-              </NavDropdown>
+              <Nav.Link onClick={() => { navigate('/') }}>Main</Nav.Link>
+              <Nav.Link onClick={() => { navigate('/login') }}>Login</Nav.Link>
+              <Nav.Link onClick={() => { navigate('/signUp') }}>signUp</Nav.Link>
             </Nav>
           </Container>
         </Navbar>
+        <Navbar style={{ backgroundColor: 'purple' }} data-bs-theme="dark">
+          <Container>
+            <Nav style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', width: '100%' }}>
+              <Navbar.Brand className="mx-2">처음처럼가격</Navbar.Brand>
+              <Nav.Link onClick={sortByPriceAsc}>&#9651;</Nav.Link>
+              <Nav.Link onClick={sortByPriceDesc}>&#9661;</Nav.Link>
+              <Navbar.Brand className="mx-2">물건명</Navbar.Brand>
+              <Nav.Link onClick={sortByNameAsc}>&#9651;</Nav.Link> {/* 오름차순 */}
+              <Nav.Link onClick={sortByNameDesc}>&#9661;</Nav.Link> {/* 내림차순 */}
+            </Nav>
+          </Container>
+        </Navbar>
+
+
       </div>
     </>
   )
