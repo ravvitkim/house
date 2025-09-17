1e2ac99003c35917480ca122b582dff21f7d5f96
ravvitkim <mirkim6086@naver.com>
2025-09-17 18:07:45 +0900

    닫기버튼 만듦

--- src/Room/index.jsx
+++ src/Room/index.jsx
@@ -1,13 +1,24 @@
+import { useState } from "react";
+
 function Room({ room }) {
   const image = `/images/room${room.id}.jpg`; // id를 사용해서 이미지 경로 생성
+  const  [click, setClick] = useState(false);
 
   return (
-    <>
+    <div className="room-box">
       <h2>{room.title}</h2>
       <p>{room.content}</p>
       <p>가격 : {room.price}</p>
-      <img src={image} width="80%" alt={room.title} />
-    </>
+      <img src={image} width="80%" alt={room.title}  onClick={() => setClick(true)}/>
+      {click && (
+        <button
+          className="close-button"
+          onClick={() => setClick(false)}
+        >
+          닫기
+        </button>
+      )}
+    </div>
   );
 }
 
